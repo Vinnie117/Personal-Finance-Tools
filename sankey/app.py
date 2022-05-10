415fe8ddbf6d3df279049f087be802bdaca37aca
Vinnie117 <hhang@hotmail.de>
2022-05-10 22:46:29 +0200

    make it better

--- sankey/app.py
+++ sankey/app.py
@@ -1,26 +1,67 @@
 import plotly.graph_objects as go
+import yaml
 
-# from dash import Dash, dcc, html, Input, Output
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
+
+from dash import Dash, dcc, html, Input, Output
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+# fig = go.Figure(data=[go.Sankey(
+#     node = dict(
+#       pad = 15,
+#       thickness = 20,
+#       line = dict(color = "black", width = 0.5),
+#       label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+#       color = "blue"
+#     ),
+#     link = dict(
+#       source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
+#       target = [2, 3, 3, 4, 4, 5],
+#       value = [8, 4, 2, 8, 4, 2]
+#   ))])
+
+# fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+# fig.show()
+
+# load data
+with open('A:\Projects\Personal-Finance-Tools\sankey\complex_sankey_data.yaml') as f:
+    data = yaml.safe_load(f)
+
+# override gray link colors with 'source' colors
+opacity = 0.4
+# change 'magenta' to its 'rgba' value to add opacity
+data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
+data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
+                                    for src in data['data'][0]['link']['source']]
+
+
 
 fig = go.Figure(data=[go.Sankey(
+    valueformat = data['data'][0]['valueformat'],
+    valuesuffix = data['data'][0]['valuesuffix'],
+    # Define nodes
     node = dict(
       pad = 15,
-      thickness = 20,
+      thickness = 15,
       line = dict(color = "black", width = 0.5),
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-      color = "blue"
+      label =  data['data'][0]['node']['label'],
+      color =  data['data'][0]['node']['color']
     ),
+    # Add links
     link = dict(
-      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
-  ))])
+      source =  data['data'][0]['link']['source'],
+      target =  data['data'][0]['link']['target'],
+      value =  data['data'][0]['link']['value'],
+      label =  data['data'][0]['link']['label'],
+      color =  data['data'][0]['link']['color']
+))])
 
-fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+fig.update_layout(title_text = data['layout']['title']['text'],
+                  font_size = data['layout']['font']['size'],
+                  width = data['layout']['width'],
+                  height = data['layout']['height'])
 fig.show()
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
