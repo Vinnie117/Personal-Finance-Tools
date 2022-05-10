51434025720989024efe70a6bbed3fccd757c358
Vinnie117 <hhang@hotmail.de>
2022-05-10 23:11:07 +0200

    old app.py without sankey

--- app.py
+++ app.py
@@ -3,86 +3,13 @@ import plotly.graph_objects as go
 import yaml
 
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-# load data
-with open('A:\Projects\Personal-Finance-Tools\sankey\complex_sankey_data.yaml') as f:
-    data = yaml.safe_load(f)
 
-# # override gray link colors with 'source' colors
-# opacity = 0.4
-# # change 'magenta' to its 'rgba' value to add opacity
-# data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
-# data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
-#                                     for src in data['data'][0]['link']['source']]
-
-# fig = go.Figure(data=[go.Sankey(
-#     valueformat = data['data'][0]['valueformat'],
-#     valuesuffix = data['data'][0]['valuesuffix'],
-#     # Define nodes
-#     node = dict(
-#       pad = 15,
-#       thickness = 15,
-#       line = dict(color = "black", width = 0.5),
-#       label =  data['data'][0]['node']['label'],
-#       color =  data['data'][0]['node']['color']
-#     ),
-#     # Add links
-#     link = dict(
-#       source =  data['data'][0]['link']['source'],
-#       target =  data['data'][0]['link']['target'],
-#       value =  data['data'][0]['link']['value'],
-#       label =  data['data'][0]['link']['label'],
-#       color =  data['data'][0]['link']['color']
-# ))])
-
-# fig.update_layout(title_text = data['layout']['title']['text'],
-#                   font_size = data['layout']['font']['size'],
-#                   width = data['layout']['width'],
-#                   height = data['layout']['height'])
-# fig.show()
-
-
-#######################################################################################
-
-
-# override gray link colors with 'source' colors
-opacity = 0.4
-# change 'magenta' to its 'rgba' value to add opacity
-data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
-data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
-                                    for src in data['data'][0]['link']['source']]
-
-fig = go.Figure(data=[go.Sankey(
-    valueformat = data['data'][0]['valueformat'],
-    valuesuffix = data['data'][0]['valuesuffix'],
-    # Define nodes
-    node = dict(
-      pad = 15,
-      thickness = 15,
-      line = dict(color = "black", width = 0.5),
-      label =  data['data'][0]['node']['label'],
-      color =  data['data'][0]['node']['color']
-    ),
-    # Add links
-    link = dict(
-      source =  data['data'][0]['link']['source'],
-      target =  data['data'][0]['link']['target'],
-      value =  data['data'][0]['link']['value'],
-      label =  data['data'][0]['link']['label'],
-      color =  data['data'][0]['link']['color']
-))])
-
-fig.update_layout(title_text = data['layout']['title']['text'],
-                  font_size = data['layout']['font']['size'],
-                  width = data['layout']['width'],
-                  height = data['layout']['height'])
-fig.show()
 
 app.layout = html.Div([
     html.H2('Hello World'),
@@ -90,11 +17,7 @@ app.layout = html.Div([
         'LA',
         id='dropdown'
     ),
-    html.Div(id='display-value'),
-    dcc.Graph(
-        id='graph1',
-        figure=fig
-        )
+    html.Div(id='display-value')
 ])
 
 @app.callback(Output('display-value', 'children'),
