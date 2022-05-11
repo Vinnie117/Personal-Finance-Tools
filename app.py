dbaae456e7829affe0649f6a68e12230e94e9006
Vinnie117 <hhang@hotmail.de>
2022-05-11 21:29:26 +0200

    deploy sankey - wip

--- app.py
+++ app.py
@@ -1,34 +1,76 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
 import yaml
+import urllib.request
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+# app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
-
-
-
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL', 'Orschel'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
-])
-
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+# server = app.server
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+# app.layout = html.Div([
+#     html.H2('Hello World'),
+#     dcc.Dropdown(['LA', 'NYC', 'MTL', 'Orschel'],
+#         'LA',
+#         id='dropdown'
+#     ),
+#     html.Div(id='display-value')
+# ])
+
+# @app.callback(Output('display-value', 'children'),
+#                 [Input('dropdown', 'value')])
+# def display_value(value):
+#     return f'You have selected {value}'
+
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
 #################################################################
 
+app = Dash(__name__)
 
+# get data 
+url = 'https://raw.githubusercontent.com/Vinnie117/personal-finance-tools/main/sankey/complex_sankey_data.yaml'
+response = urllib.request.urlopen(url)
+data = yaml.safe_load(response.read())
+
+fig = go.Figure(data=[go.Sankey(
+    valueformat = data['data'][0]['valueformat'],
+    valuesuffix = data['data'][0]['valuesuffix'],
+    # Define nodes
+    node = dict(
+      pad = 15,
+      thickness = 15,
+      line = dict(color = "black", width = 0.5),
+      label =  data['data'][0]['node']['label'],
+      color =  data['data'][0]['node']['color']
+    ),
+    # Add links
+    link = dict(
+      source =  data['data'][0]['link']['source'],
+      target =  data['data'][0]['link']['target'],
+      value =  data['data'][0]['link']['value'],
+      label =  data['data'][0]['link']['label'],
+      color =  data['data'][0]['link']['color']
+))])
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
