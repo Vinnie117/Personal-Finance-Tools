c3d16502459df4f0abaf407a0db825bc3421988f
Vinnie117 <hhang@hotmail.de>
2022-05-13 22:55:03 +0200

    callback sankey function - wip

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ url = 'https://raw.githubusercontent.com/Vinnie117/personal-finance-tools/main/s
 response = urllib.request.urlopen(url)
 data = yaml.safe_load(response.read())
 
+# Built Sankey with plotly
 fig = go.Figure(data=[go.Sankey(
     valueformat = data['data'][0]['valueformat'],
     valuesuffix = data['data'][0]['valuesuffix'],
@@ -36,6 +37,8 @@ fig.update_layout(title_text = data['layout']['title']['text'], # ABC Test
                   width = data['layout']['width'],
                   height = data['layout']['height'])
 
+
+# Create web app with dash
 app.layout = html.Div(children=[
     html.H1(children='Hello World'),
     html.Div(children='''
@@ -44,33 +47,38 @@ app.layout = html.Div(children=[
 
     # Sankey Plot
     dcc.Graph(
-        id='example-graph',
+        id='sankey_graph',
         figure=fig
     ),
 
     # Textbox for Sankey input
     dcc.Textarea(
+    id='sankey',
+    value='Gehalt [100] Miete \nGehalt [70] Konsum\nGehalt [30] Sparen',    
     placeholder='Enter a value...',
-    value='This is a TextArea component',
     style={'width': '100%'}
     ),
 
     # Test Button -> Button to submit info from Textarea to Sankey
-    html.Div(dcc.Input(id='input-box', type='text')),
-    html.Button('Submit', id='button_1'),
-    html.Div(id='output-container-button',
-             children='Enter a value and press submit'),
+    html.Button('Submit', id='button_1')
 ])
 
 @app.callback(
-    Output('output-container-button', 'children'),
-    [Input('button_1', 'n_clicks')],
-    [State('input-box', 'value')])
-def update_output(n_clicks, value):
-    return 'The input value was "{}" and the button has been clicked {} times'.format(
-        value,
-        n_clicks
-    )
+    # Output(component_id='output-container-button', component_property='children'),
+    # [Input(component_id='button_1', component_property='n_clicks')],
+    # [State('input-box', 'value')])
+    Output(component_id='sankey_graph', component_property='figure'),
+    [Input(component_id='sankey', component_property='value')],
+    [State('sankey', 'value')])
+def update_sankey(text):
+    return 'The input value was "{}" and the button has been clicked {} times'.format(text)
+
+# def updated_sankey() needs to be in callback!
+
+
+# klären
+# -> wie genau erhält die Funktion ihre Argumente im Callback?
+# -> wie / wo werden Funktionsinputs erzeugt?
 
 
 
