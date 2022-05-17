6e943965c35410d4e65431e39aad68eb4f7e8506
Vinnie117 <hhang@hotmail.de>
2022-05-17 21:31:44 +0200

    clean up and structure

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, dcc, html, Input, Output, State
 import plotly.graph_objects as go
 import yaml
 import urllib.request
+import sankey.plot
 from sankey import functions
 
 app = Dash(__name__)
@@ -19,44 +20,18 @@ data = yaml.safe_load(response.read())
 
 ##############################################################
 
-# Built Default Sankey with plotly
-fig = go.Figure(data=[go.Sankey(
-    valueformat = data['data'][0]['valueformat'],
-    valuesuffix = data['data'][0]['valuesuffix'],
-    # Define nodes
-    node = dict(
-      pad = 15,
-      thickness = 15,
-      line = dict(color = "black", width = 0.5),
-      label =  data['data'][0]['node']['label'],
-      color =  data['data'][0]['node']['color']
-    ),
-    # Add links
-    link = dict(
-      source =  data['data'][0]['link']['source'],
-      target =  data['data'][0]['link']['target'],
-      value =  data['data'][0]['link']['value'],
-      label =  data['data'][0]['link']['label'],
-      color =  data['data'][0]['link']['color']
-))])
-
-fig.update_layout(title_text = data['layout']['title']['text'], 
-                  font_size = data['layout']['font']['size'],
-                  width = data['layout']['width'],
-                  height = data['layout']['height'])
-
 
 # Create web app with dash
 app.layout = html.Div(children=[
-    html.H1(children='Hello World'),
+    html.H1(children='Visualization tools for personal finances'),
     html.Div(children='''
-        Dashboard: Dies ist eine Testanwendung.
+        Dashboard: Sankey for payment flows.
     '''),
 
     # Sankey Plot
     dcc.Graph(
         id='sankey_graph',
-        figure=fig,
+        figure=sankey.plot.fig,
         style={'width': '640px', 'height': '400px'}
     ),
 
---
+++ sankey/plot.py
@@ -0,0 +1,54 @@
+import plotly.graph_objects as go
+import yaml
+
+#######################################################
+# load data
+with open('A:\Projects\Personal-Finance-Tools\sankey\data.yaml') as f:
+    data = yaml.safe_load(f)
+
+# url = 'https://raw.githubusercontent.com/Vinnie117/personal-finance-tools/main/sankey/complex_sankey_data.yaml'
+# response = urllib.request.urlopen(url)
+# data = yaml.safe_load(response.read())
+#######################################################
+
+# Built Default Sankey with plotly
+fig = go.Figure(data=[go.Sankey(
+    valueformat = data['data'][0]['valueformat'],
+    valuesuffix = data['data'][0]['valuesuffix'],
+    # Define nodes
+    node = dict(
+      pad = 15,
+      thickness = 15,
+      line = dict(color = "black", width = 0.5),
+      label =  data['data'][0]['node']['label'],
+#     color =  data['data'][0]['node']['color'] 
+#     x = [0.5],      # manual position of the node! -> can match with annotation in add_annotation()
+#     y = [0.8]
+    ),
+    # Add links
+    link = dict(
+      source =  data['data'][0]['link']['source'],
+      target =  data['data'][0]['link']['target'],
+      value =  data['data'][0]['link']['value'],
+      label =  data['data'][0]['link']['label'],
+      color =  data['data'][0]['link']['color']
+))])
+
+fig.update_layout(title_text = data['layout']['title']['text'], 
+                  font_size = data['layout']['font']['size'],
+                  width = data['layout']['width'],
+                  height = data['layout']['height'])
+
+# # distribute annotation evenly?
+# # -> plot is in space between coordinates [0,1] for x and y -> specify annotations relatively
+# fig.add_annotation(dict(font=dict(color='black',size=10),
+#                                         x=0.9,
+#                                         y=0.725,
+#                                         showarrow=False,
+#                                         text=data['data'][0]['link']['value'][0],
+#                                         textangle=0,
+#                                         xanchor='left',
+#                                         xref="paper",
+#                                         yref="paper"))
+
+#fig.show()
